466b0c8fa762339509dc2266da2a02d3de0d0c80
Jed Duffey <jedduffey@gmail.com>
2018-09-11 22:06:54 -0400

    Print hex digest

--- hashtest.py
+++ hashtest.py
@@ -6,25 +6,28 @@ import time
 print()
 
 secret = 'ABCDEFGHIJKLMNOP'
-print('Secret:          ' + secret)
-stringToByets = bytes(secret, 'ascii')
+print('Secret:               ' + secret)
 
+stringToByets = bytes(secret, 'utf-8')
 m = hashlib.sha256()
 m.update(stringToByets)
-print('SHA256 digest:   ' + str(m.digest()))
-digestSize = m.digest_size
-print('Digest size:     ' + str(digestSize))
-blockSize = m.block_size
-print('Block size:      ' + str(blockSize))
 
+hexDigest = m.hexdigest()
+print('SHA256 hex digest:    ' + str(hexDigest))
+
+digestSize = m.digest_size
+print('Digest size:          ' + str(digestSize))
+
+blockSize = m.block_size
+print('Block size:           ' + str(blockSize))
 
 currentUnixTime = time.time()
 currentUnixSeconds = int(currentUnixTime)
-print('Unix time:       ' + str(currentUnixTime))
-print('Unix seconds:    ' + str(currentUnixSeconds))
+print('Unix time:            ' + str(currentUnixTime))
+print('Unix seconds:         ' + str(currentUnixSeconds))
 
 period = 2
 chunkedTime = int(currentUnixSeconds / period)
-print('Chunked time:    ' + str(chunkedTime))
+print('Chunked time:         ' + str(chunkedTime))
 
 print()
