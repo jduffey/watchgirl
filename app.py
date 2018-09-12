9b0712f08744ac9f6f674b99ed9d313da8a5b4be
Jed Duffey <jedduffey@gmail.com>
2018-09-12 13:19:19 -0400

    MVP milestone: QR codes display with new hashing algorithm

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import jedhash
 
+import pyqrcode
+
+from PIL import Image
+
+import time
+
 secretInput = 'ABCDEFGHIJKLMNOP'
 periodInput = 1
 
@@ -14,9 +20,20 @@ consolePrintBuffer('BEGIN')
 print('Secret:         ' + secretInput)
 print('Period (sec):   ' + str(periodInput))
 
-jedhash.returnTheHash(secretInput, periodInput)
+for i in range(10):
 
-outerHash = jedhash.returnTheHash(secretInput, periodInput)
-print('Final hash:     ' + outerHash)
+    outerHash = jedhash.returnTheHash(secretInput, periodInput)
+    print('Final hash:     ' + outerHash)
+
+    qrCurrent = pyqrcode.create(str(outerHash))
+    qrCurrent.png('qrCurrent.png', scale = 20)
+    print('(Debug: image written)')
+
+    with Image.open('qrCurrent.png') as img:
+        print('BEFORE Command to open image')
+        img.show()
+        print('AFTER Command to open image')
+
+    time.sleep(1)
 
 consolePrintBuffer('END')
