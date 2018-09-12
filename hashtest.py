8afea0aef365f08d4b87b056a8f76969e05e99af
Jed Duffey <jedduffey@gmail.com>
2018-09-12 11:56:32 -0400

    Refactor; create main function

--- hashtest.py
+++ hashtest.py
@@ -2,9 +2,7 @@ import hashlib
 import datetime
 import time
 
-print()
-print('*** BEGIN ***')
-print()
+
 
 """ CONSTANTS """
 secretStringCONSTANT = 'ABCDEFGHIJKLMNOP'
@@ -31,46 +29,50 @@ def getChunkedTime(period):
     chunkedTime = int(currentUnixSeconds / period)
     return chunkedTime
 
-def setInnerString(secret):
+def setInnerString(secret, chunkedTime):
     innerString = secret + str(chunkedTime)
     return innerString
 
-""" TO BE FILED UNDER MAIN """
+def main():
+
+    print()
+    print('*** BEGIN ***')
+    print()
 
 # Set and print the secret
-secret = setSecret(secretStringCONSTANT)
-print('Secret:               ' + secret)
+    secret = setSecret(secretStringCONSTANT)
+    print('Secret:               ' + secret)
 
 # Set the period
-period = setPeriod(periodIntegerCONSTANT)
+    period = setPeriod(periodIntegerCONSTANT)
 
 # Assign and print chunked time
-chunkedTime = getChunkedTime(period)
-print('Chunked time:         ' + str(chunkedTime))
+    chunkedTime = getChunkedTime(period)
+    print('Chunked time:         ' + str(chunkedTime))
 
 # Hash the secret and print the digest (NOT RELEVANT TO FINAL PRODUCT)
-hasedSecret = hashTheInput(secret)
-hexDigestOfSecret = hasedSecret.hexdigest()
-print('SHA256 secret digest: ' + str(hexDigestOfSecret))
+    hasedSecret = hashTheInput(secret)
+    hexDigestOfSecret = hasedSecret.hexdigest()
+    print('SHA256 secret digest: ' + str(hexDigestOfSecret))
 
 # Set inner string
-innerString = setInnerString(secret)
+    innerString = setInnerString(secret, chunkedTime)
 
 # Operations for inner hash
-shaInner = hashTheInput(innerString)
+    shaInner = hashTheInput(innerString)
 
 # Print inner string and digest (NOT RELEVANT TO FINAL PRODUCT)
-print('innerString:          ' + innerString)
-hashedInnerAsHex = shaInner.hexdigest()
-print('Inner hash:           ' + str(hashedInnerAsHex))
+    print('innerString:          ' + innerString)
+    hashedInnerAsHex = shaInner.hexdigest()
+    print('Inner hash:           ' + str(hashedInnerAsHex))
 
 # Operations for outer hash
-shaOuter = hashTheInput(secret + hashedInnerAsHex)
+    shaOuter = hashTheInput(secret + hashedInnerAsHex)
 
 # Print the outer digest
-outerHexDigest = shaOuter.hexdigest()
-print('Outer hash:           ' + outerHexDigest)
+    outerHexDigest = shaOuter.hexdigest()
+    print('Outer hash:           ' + outerHexDigest)
 
-print()
-print('*** END ***')
-print()
+    print()
+    print('*** END ***')
+    print()
