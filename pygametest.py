b4de01072593fcbd1d5ecabfe02edd62735098e3
Jed Duffey <jedduffey@gmail.com>
2018-10-31 09:59:30 -0400

    Squares display left to right in a row

--- pygametest.py
+++ pygametest.py
@@ -12,6 +12,9 @@ pygame.init()
 
 APP_SQUARE_SIZE = 600
 
+APP_X_SIZE = 400
+APP_Y_SIZE = 100
+
 myRed = (255, 0, 0)
 myBlue = (0, 0, 255)
 myGreen = (0, 255, 0)
@@ -20,12 +23,17 @@ myWhite = (255, 255, 255)
 myLightRed = (255, 180, 180)
 myLightBLue = (190, 190, 255)
 
-squareOne = (0, 0, APP_SQUARE_SIZE/2, APP_SQUARE_SIZE/2)
-squareTwo = (APP_SQUARE_SIZE/2, 0, APP_SQUARE_SIZE, APP_SQUARE_SIZE/2)
-squareThree = (0, APP_SQUARE_SIZE/2, APP_SQUARE_SIZE/2, APP_SQUARE_SIZE)
-squareFour = (APP_SQUARE_SIZE/2, APP_SQUARE_SIZE/2, APP_SQUARE_SIZE, APP_SQUARE_SIZE)
+# squareOne = (0, 0, APP_SQUARE_SIZE/2, APP_SQUARE_SIZE/2)
+# squareTwo = (APP_SQUARE_SIZE/2, 0, APP_SQUARE_SIZE, APP_SQUARE_SIZE/2)
+# squareThree = (0, APP_SQUARE_SIZE/2, APP_SQUARE_SIZE/2, APP_SQUARE_SIZE)
+# squareFour = (APP_SQUARE_SIZE/2, APP_SQUARE_SIZE/2, APP_SQUARE_SIZE, APP_SQUARE_SIZE)
 
-screen = pygame.display.set_mode((APP_SQUARE_SIZE, APP_SQUARE_SIZE))
+squareOne = (0, 0, 1 * APP_X_SIZE/4, APP_Y_SIZE)
+squareTwo = (1 * APP_X_SIZE/4, 0, 2 * APP_X_SIZE/4, APP_Y_SIZE)
+squareThree = (2 * APP_X_SIZE/4, 0, 3 * APP_X_SIZE/4, APP_Y_SIZE)
+squareFour = (3 * APP_X_SIZE/4, 0, 4 * APP_X_SIZE/4, APP_Y_SIZE)
+
+screen = pygame.display.set_mode((APP_X_SIZE, APP_Y_SIZE))
 pygame.display.set_caption('Identifier #580CD2E889BD - Onett Art Museum Main Entrance')
 pygame.mouse.set_visible(True)
 #pygame.mouse.set_visible(False)
