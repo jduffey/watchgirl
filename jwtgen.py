97a4dedc6eaa9a6b5b567f064f6d213c6b1c1967
Jed Duffey <jedduffey@gmail.com>
2018-11-19 11:26:09 -0500

    Remove irrelevant files

--- jwtgen.py
+++
@@ -1,8 +0,0 @@
-import json
-import jwt
-import datetime
-
-
-def genjwt(iss, desc, iat, exp, secret, alg):
-    generated_JWT = jwt.encode({'iss': iss, 'desc': desc, 'iat': iat, 'exp': exp}, secret, algorithm=alg)
-    return generated_JWT
