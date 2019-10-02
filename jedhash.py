7fa38f00205a703c4fc794342ab6b32ecb4b1c1f
Jed Duffey <jedduffey@gmail.com>
2019-10-02 19:27:29 -0400

    update to pythonic naming conventions

--- jedhash.py
+++ jedhash.py
@@ -3,31 +3,31 @@ import datetime
 import time
 
 
-def setSecret(secretString):
-    secretString = secretString
-    return secretString
+def set_secret(secret_string):
+    secret_string = secret_string
+    return secret_string
 
-def setPeriod(periodInteger):
-    period = periodInteger
+def set_period(period_integer):
+    period = period_integer
     return period
 
-def hashTheInput(stringInput):
-    stringToByets = bytes(stringInput, 'utf-8')
-    hashedInput = hashlib.sha256()
-    hashedInput.update(stringToByets)
-    return hashedInput
+def hash_the_input(string_input):
+    string_to_bytes = bytes(string_input, 'utf-8')
+    hashed_input = hashlib.sha256()
+    hashed_input.update(string_to_bytes)
+    return hashed_input
 
-def getChunkedTime(period, offset):
-    timeToUse = time.time() + offset
-    unixSecToUse = int(timeToUse)
-    chunkedTime = int(unixSecToUse / period)
-    return chunkedTime
+def get_chunked_time(period, offset):
+    time_to_use = time.time() + offset
+    unix_sec_to_use = int(time_to_use)
+    chunked_time = int(unix_sec_to_use / period)
+    return chunked_time
 
-def setInnerString(secret, chunkedTime):
-    innerString = secret + str(chunkedTime)
-    return innerString
+def set_inner_string(secret, chunked_time):
+    inner_string = secret + str(chunked_time)
+    return inner_string
 
-def returnTheHash(secretInput, periodInput, offset):
+def return_the_hash(secret_input, period_input, offset):
 
     """
     This algorithm is a variation of the one found here:
@@ -40,20 +40,20 @@ def returnTheHash(secretInput, periodInput, offset):
     hmac = SHA1(secret + SHA1(secret + input))
     """
 
-    secret = setSecret(secretInput)
+    secret = set_secret(secret_input)
     
-    period = setPeriod(periodInput)
+    period = set_period(period_input)
 
-    chunkedTime = getChunkedTime(period, offset)
+    chunked_time = get_chunked_time(period, offset)
 
-    innerString = setInnerString(secret, chunkedTime)
+    inner_string = set_inner_string(secret, chunked_time)
 
-    shaInner = hashTheInput(innerString)
+    sha_inner = hash_the_input(inner_string)
 
-    hashedInnerAsHex = shaInner.hexdigest()
+    hashed_inner_as_hex = sha_inner.hexdigest()
 
-    shaOuter = hashTheInput(secret + hashedInnerAsHex)
+    sha_outer = hash_the_input(secret + hashed_inner_as_hex)
 
-    outerHexDigest = shaOuter.hexdigest()
+    outer_hex_digest = sha_outer.hexdigest()
 
-    return outerHexDigest
+    return outer_hex_digest
--- signalmaker.py
+++ signalmaker.py
@@ -2,27 +2,27 @@ import pygame
 from pygame.locals import *
 import time
 import jedhash
-from config import config
+from config import constants
 
 
-SECRET = config['SECRET']
-PERIOD_IN_SECONDS = config['PERIOD_IN_SECONDS']
-WINDOW_TITLE = config['WINDOW_TITLE']
-BORDER_THICKNESS = config['BORDER_THICKNESS']
-ICON_SIZE_X = config['ICON_SIZE_X']
-ICON_SIZE_Y = config['ICON_SIZE_Y']
-NUMBER_OF_COLUMNS = config['NUMBER_OF_COLUMNS']
-NUMBER_OF_ROWS = config['NUMBER_OF_ROWS']
-MY_RED = config['MY_RED']
-MY_ORANGE = config['MY_ORANGE']
-MY_YELLOW = config['MY_YELLOW']
-MY_GREEN = config['MY_GREEN']
-MY_BLUE = config['MY_BLUE']
-MY_VIOLET = config['MY_VIOLET']
-MY_BROWN = config['MY_BROWN']
-MY_GREY = config['MY_GREY']
-MY_BLACK = config['MY_BLACK']
-MY_WHITE = config['MY_WHITE']
+SECRET = constants['SECRET']
+PERIOD_IN_SECONDS = constants['PERIOD_IN_SECONDS']
+WINDOW_TITLE = constants['WINDOW_TITLE']
+BORDER_THICKNESS = constants['BORDER_THICKNESS']
+ICON_SIZE_X = constants['ICON_SIZE_X']
+ICON_SIZE_Y = constants['ICON_SIZE_Y']
+NUMBER_OF_COLUMNS = constants['NUMBER_OF_COLUMNS']
+NUMBER_OF_ROWS = constants['NUMBER_OF_ROWS']
+MY_RED = constants['MY_RED']
+MY_ORANGE = constants['MY_ORANGE']
+MY_YELLOW = constants['MY_YELLOW']
+MY_GREEN = constants['MY_GREEN']
+MY_BLUE = constants['MY_BLUE']
+MY_VIOLET = constants['MY_VIOLET']
+MY_BROWN = constants['MY_BROWN']
+MY_GREY = constants['MY_GREY']
+MY_BLACK = constants['MY_BLACK']
+MY_WHITE = constants['MY_WHITE']
 
 number_color_dict = {
     '0': MY_RED,
@@ -44,39 +44,39 @@ number_color_dict = {
 }
 
 
-def getColor(digitToUseForColor):
-    return number_color_dict[digitToUseForColor]
+def get_color(digit_to_use_for_color):
+    return number_color_dict[digit_to_use_for_color]
 
-def drawHorizontalBorders():
+def draw_horizontal_borders():
     for i in range(0, NUMBER_OF_ROWS + 1):
-        topLeftX = 0
-        topLeftY = i * ( BORDER_THICKNESS + ICON_SIZE_Y )
-        bottomRightX = APP_X_SIZE
-        bottomRightY = BORDER_THICKNESS
-        theRectangleToDraw = (topLeftX, topLeftY, bottomRightX, bottomRightY)
-        pygame.draw.rect(screen, MY_BLACK, theRectangleToDraw)
+        top_left_X = 0
+        top_left_Y = i * ( BORDER_THICKNESS + ICON_SIZE_Y )
+        bottom_right_X = APP_X_SIZE
+        bottom_right_Y = BORDER_THICKNESS
+        the_rectangle_to_draw = (top_left_X, top_left_Y, bottom_right_X, bottom_right_Y)
+        pygame.draw.rect(screen, MY_BLACK, the_rectangle_to_draw)
 
-def drawVerticalBorders():
+def draw_vertical_borders():
     for i in range(0, NUMBER_OF_COLUMNS + 1):
-        topLeftX = i * ( BORDER_THICKNESS + ICON_SIZE_X )
-        topLeftY = 0
-        bottomRightX = BORDER_THICKNESS
-        bottomRightY = APP_Y_SIZE
-        theRectangleToDraw = (topLeftX, topLeftY, bottomRightX, bottomRightY)
-        pygame.draw.rect(screen, MY_BLACK, theRectangleToDraw)
+        top_left_X = i * ( BORDER_THICKNESS + ICON_SIZE_X )
+        top_left_Y = 0
+        bottom_right_X = BORDER_THICKNESS
+        bottom_right_Y = APP_Y_SIZE
+        the_rectangle_to_draw = (top_left_X, top_left_Y, bottom_right_X, bottom_right_Y)
+        pygame.draw.rect(screen, MY_BLACK, the_rectangle_to_draw)
 
-def drawColorIcon(whichColumn, whichRow, digitToUseForColor):
-    topLeftX = BORDER_THICKNESS + whichColumn * ( ICON_SIZE_X + BORDER_THICKNESS )
-    topLeftY = BORDER_THICKNESS + whichRow * ( ICON_SIZE_Y + BORDER_THICKNESS )
-    bottomRightX = ICON_SIZE_X
-    bottomRightY = ICON_SIZE_Y
-    theRectangleToDraw = (topLeftX, topLeftY, bottomRightX, bottomRightY)
-    pygame.draw.rect(screen, getColor(digitToUseForColor), theRectangleToDraw)
+def draw_color_icon(which_column, which_row, digit_to_use_for_color):
+    top_left_X = BORDER_THICKNESS + which_column * ( ICON_SIZE_X + BORDER_THICKNESS )
+    top_left_Y = BORDER_THICKNESS + which_row * ( ICON_SIZE_Y + BORDER_THICKNESS )
+    bottom_right_X = ICON_SIZE_X
+    bottom_right_Y = ICON_SIZE_Y
+    the_rectangle_to_draw = (top_left_X, top_left_Y, bottom_right_X, bottom_right_Y)
+    pygame.draw.rect(screen, get_color(digit_to_use_for_color), the_rectangle_to_draw)
 
-def generateDigest(timeOffset):
-    return jedhash.returnTheHash(SECRET, PERIOD_IN_SECONDS, timeOffset)
+def generate_digest(time_offset):
+    return jedhash.return_the_hash(SECRET, PERIOD_IN_SECONDS, time_offset)
 
-def establishStartTime():
+def establish_start_time():
     # force loops to start halfway between periods
     return time.time() - (time.time() % PERIOD_IN_SECONDS) + PERIOD_IN_SECONDS / 2
 
@@ -87,63 +87,63 @@ APP_Y_SIZE = BORDER_THICKNESS + NUMBER_OF_ROWS * ( ICON_SIZE_Y + BORDER_THICKNES
 screen = pygame.display.set_mode((APP_X_SIZE, APP_Y_SIZE))
 pygame.display.set_caption(WINDOW_TITLE)
 pygame.mouse.set_visible(True)
-rectangleThatIsTheSizeOfTheScreen = pygame.Surface(screen.get_size())
-rectangleThatIsTheSizeOfTheScreen.fill(MY_WHITE)
-screen.blit(rectangleThatIsTheSizeOfTheScreen, (0, 0))
+rectangle_that_is_the_size_of_the_screen = pygame.Surface(screen.get_size())
+rectangle_that_is_the_size_of_the_screen.fill(MY_WHITE)
+screen.blit(rectangle_that_is_the_size_of_the_screen, (0, 0))
 pygame.display.flip()
 
 pygame.init()
-isDrawingActive = True
+is_drawing_active = True
 
 
-startTime = establishStartTime()
-print(startTime)
+start_time = establish_start_time()
+print(start_time)
 
-while isDrawingActive:
+while is_drawing_active:
 
     print()
 
-    loopStartTime = time.time()
-    print('Loop start: ' + str(loopStartTime))
+    loop_start_time = time.time()
+    print('Loop start: ' + str(loop_start_time))
 
-    digestsGeneratedThisLoop = 0
+    digests_generated_this_loop = 0
 
-    outerList = []
+    outer_list = []
 
-    for whichColumn in range(0, NUMBER_OF_COLUMNS):
+    for which_column in range(0, NUMBER_OF_COLUMNS):
 
-        innerList = []
+        inner_list = []
 
-        for whichRow in range(0, NUMBER_OF_ROWS):
+        for which_row in range(0, NUMBER_OF_ROWS):
 
-            digestTimeOffset = PERIOD_IN_SECONDS * (whichColumn - whichRow)
+            digest_time_offset = PERIOD_IN_SECONDS * (which_column - which_row)
 
-            digestToUseForCurrentIcon = generateDigest(digestTimeOffset)
-            digestsGeneratedThisLoop += 1
+            digest_to_use_for_current_icon = generate_digest(digest_time_offset)
+            digests_generated_this_loop += 1
 
-            digitToUseForColor = digestToUseForCurrentIcon[0]
+            digit_to_use_for_color = digest_to_use_for_current_icon[0]
 
-            drawColorIcon(whichColumn, whichRow, digitToUseForColor)
+            draw_color_icon(which_column, which_row, digit_to_use_for_color)
 
-            innerList.append(digitToUseForColor)
+            inner_list.append(digit_to_use_for_color)
 
-        outerList.append(innerList)
+        outer_list.append(inner_list)
 
-    drawHorizontalBorders()
-    drawVerticalBorders()
+    draw_horizontal_borders()
+    draw_vertical_borders()
 
     pygame.display.flip()
 
-    millisecondsOfLoop = 1000 * (time.time() - loopStartTime)
+    milliseconds_of_loop = 1000 * (time.time() - loop_start_time)
 
-    print('  ms taken: ' + str(millisecondsOfLoop))
-    print('   Digests: ' + str(digestsGeneratedThisLoop))
-    for list in outerList:
+    print('  ms taken: ' + str(milliseconds_of_loop))
+    print('   digests: ' + str(digests_generated_this_loop))
+    for list in outer_list:
         print(list)
 
     # if the 'X' button is pressed the window should close:
-    Geesh = pygame.event.get()
-    if len(Geesh) > 0:
-        if Geesh[0].type == QUIT: isDrawingActive = False
+    gotten_events = pygame.event.get()
+    if len(gotten_events) > 0:
+        if gotten_events[0].type == QUIT: is_drawing_active = False
 
-    time.sleep(PERIOD_IN_SECONDS - ((time.time() - startTime) % PERIOD_IN_SECONDS))
+    time.sleep(PERIOD_IN_SECONDS - ((time.time() - start_time) % PERIOD_IN_SECONDS))
