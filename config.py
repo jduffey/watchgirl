ac2d42e5ebcafe1ccd7ff0c005e5acd7021e5294
Jed Duffey <jedduffey@gmail.com>
2020-02-05 22:02:59 -0500

    clean up config

--- config.py
+++ config.py
@@ -1,31 +1,22 @@
 constants = {
     'SECRET' : 'PRIVATE_KEY_GOES_HERE',
-    'PERIOD_IN_SECONDS' : 2,
-    'WINDOW_TITLE' : 'Identifier #580CD2E889BD - Onett Art Museum Main Entrance',
+    'PERIOD_IN_SECONDS' : 5,
+    'WINDOW_TITLE' : 'Bank Vault #1',
     'BORDER_THICKNESS' : 10,
     'ICON_SIZE_X' : 100,
     'ICON_SIZE_Y' : 100,
-    'NUM_COLS' : 6,
-    'NUM_ROWS' : 1,
+    'NUM_COLS' : 2,
+    'NUM_ROWS' : 2,
     'MY_RED' : (255, 0, 0),
-    'MY_ORANGE' : (255, 127, 0),
     'MY_YELLOW' : (255, 255, 0),
     'MY_GREEN' : (0, 255, 0),
     'MY_BLUE' : (0, 0, 255),
-    'MY_VIOLET' : (75, 0, 130),
-    'MY_BROWN' : (102, 51, 0),
-    'MY_GREY' : (40, 40, 40),
     'MY_BLACK' : (0, 0, 0),
-    'MY_WHITE':  (255, 255, 255),
     }
 
 number_color_dict = {
     '0': constants['MY_RED'],
-    '1': constants['MY_ORANGE'],
-    '2': constants['MY_YELLOW'],
-    '3': constants['MY_GREEN'],
-    '4': constants['MY_BLUE'],
-    '5': constants['MY_VIOLET'],
-    '6': constants['MY_BROWN'],
-    '7': constants['MY_GREY']
+    '1': constants['MY_YELLOW'],
+    '2': constants['MY_GREEN'],
+    '3': constants['MY_BLUE']
 }
