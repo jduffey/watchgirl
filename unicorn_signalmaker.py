753c000baa02ced327f3f9aa2f16dc5dd134b312
Jed Duffey <jedduffey@gmail.com>
2020-02-10 08:16:14 -0500

    WIP

--- unicorn_signalmaker.py
+++ unicorn_signalmaker.py
@@ -4,6 +4,8 @@ import totp
 from config import constants as const
 from config import number_color_dict
 from random import randint
+import subprocess
+import os
 
 
 def fill_square(bottom_left, bottom_right, top_left, top_right, r, g, b):
@@ -23,17 +25,26 @@ def sleep_between_loops():
 
 unicorn.set_layout(unicorn.AUTO)
 unicorn.rotation(0)
-unicorn.brightness(0.25) # needs to be above ~0.20
+unicorn.brightness(0.25) # needs to be above ~0.20 to activate LEDs
 width,height=unicorn.get_shape()
 
-for i in range(200):
-    x = randint(0, (width-1))
-    y = randint(0, (height-1))
-    r = randint(127, 255)
-    g = randint(127, 255)
-    b = randint(127, 255)
-    unicorn.set_pixel(x, y, r, g, b)
-    unicorn.show()
+def show_rainbow_sprinkles():
+    for i in range(300):
+        x = randint(0, (width-1))
+        y = randint(0, (height-1))
+        r = randint(127, 255)
+        g = randint(127, 255)
+        b = randint(127, 255)
+        unicorn.set_pixel(x, y, r, g, b)
+        unicorn.show()
+
+
+show_rainbow_sprinkles()
+
+print('Syncing time...')
+command = 'sudo ntpdate -s time.nist.gov'
+os.system(command) # if this fails it will still print 'Time synced.'...
+print('Time synced.')
 
 # Main app functionality below
 
@@ -81,25 +92,38 @@ while is_unicorn_active:
     print(f'TOTP: {loop_digest}')
     [print(x) for x in outer_list]
 
-    #fill_square(height//2, height, width//2, width, *get_color(str(loop_digest[0])))
-    #fill_square(height//2, height, 0, width//2, *get_color(str(loop_digest[1])))
-    #fill_square(0, height//2, width//2, width, *get_color(str(loop_digest[2])))
-    #fill_square(0, height//2, 0, width//2, *get_color(str(loop_digest[3])))
+    def do_4_squares():
+        fill_square(height//2, height, width//2, width, *get_color(str(loop_digest[0])))
+        fill_square(height//2, height, 0, width//2, *get_color(str(loop_digest[1])))
+        fill_square(0, height//2, width//2, width, *get_color(str(loop_digest[2])))
+        fill_square(0, height//2, 0, width//2, *get_color(str(loop_digest[3])))
 
-    shift = height//4
-    square_size = 2
 
-    # use 16 squares
-    for i in range(16):
-        fill_square(height - (i // 4 + 1) * shift, height - (i // 4) * shift, \
-                    width - (i % 4 + 1) * shift, width - (i % 4) * shift, \
-                    *get_color(str(loop_digest[i])))
+    def do_16_squares():
+        for i in range(16):
+            fill_square(height - (i // 4 + 1) * 2, height - (i // 4) * 2, \
+                width - (i % 4 + 1) * 2, width - (i % 4) * 2, \
+                *get_color(str(loop_digest[i])))
 
-    # use all of digest
-    #for i in range(len(loop_digest)):
-    #    unicorn.set_pixel(i % 8, i // 8, *get_color(str(loop_digest[i])))
-    #    unicorn.show()
-    #    time.sleep(pause_between_pixel)
+
+    def do_all_pixels():
+        for i in range(len(loop_digest)):
+            unicorn.set_pixel(i % 8, i // 8, *get_color(str(loop_digest[i])))
+            unicorn.show()
+
+
+    def do_this_many_squares(how_many_squares):
+        print("Do this many.")
+        func_dict = {
+            4: do_4_squares,
+            16: do_16_squares,
+            64: do_all_pixels
+            }
+        func_dict[how_many_squares]()
+
+
+    do_this_many_squares(4)
+
 
     ms_of_loop = 1000 * (time.time() - loop_start_time)
     print(f'ms: {str(ms_of_loop)}')
