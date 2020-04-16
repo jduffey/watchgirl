5f685099d6d95bf6626fb0d5538830f4c3e4842f
Jed Duffey <jedduffey@gmail.com>
2020-04-15 22:29:10 -0400

    begin refactor unicorn_singalmaker

--- unicorn_signalmaker.py
+++ unicorn_signalmaker.py
@@ -9,14 +9,14 @@ except:
         print('\n*** Unicornhat HD 16x16 ***\n')
     except:
         print('\n*** ERROR: No Unicornhat module detected ***\n')
-
-import os
-import sys
 import time
 import totp
+import schedule
+import sys
 from config import constants as const
 from config import number_color_dict
-import schedule
+from utils import get_color
+from utils import synchronize_time
 
 
 def startup_pixels():
@@ -33,52 +33,40 @@ def startup_pixels():
             time.sleep(0.01)
 
 
-def synchronize_time():
-    command = 'sudo ntpdate -s'
-    target = 'time.nist.gov'
-    print(f'Syncing time with {target}')
-    os.system(command + ' ' + target)
-
-
-def fill_square(bottom_left, bottom_right, top_left, top_right, r, g, b):
+def fill_square(bottom_left, bottom_right, top_left, top_right, color):
     for y in range(bottom_left, bottom_right):
         for x in range(top_left, top_right):
-            unicorn.set_pixel(x,y,r,g,b)
+            unicorn.set_pixel(x, y, *color)
     unicorn.show()
 
-def fill_all(r, g, b):
-    unicorn.set_all(r, g, b)
+
+def fill_all(color):
+    unicorn.set_all(*color)
     unicorn.show()
+
 
 def fill_board(loop_digest, digest_portion):
     if digest_portion == 4:
         # top left
-        fill_square(0, height//2, width//2, width, *get_color(str(loop_digest[0])))
+        fill_square(0, height//2, width//2, width, get_color(str(loop_digest[0])))
         # top right
-        fill_square(0, height//2, 0, width//2, *get_color(str(loop_digest[1])))
+        fill_square(0, height//2, 0, width//2, get_color(str(loop_digest[1])))
         # bottom left
-        fill_square(height//2, height, width//2, width, *get_color(str(loop_digest[2])))
+        fill_square(height//2, height, width//2, width, get_color(str(loop_digest[2])))
         # bottom right
-        fill_square(height//2, height, 0, width//2, *get_color(str(loop_digest[3])))
+        fill_square(height//2, height, 0, width//2, get_color(str(loop_digest[3])))
     if digest_portion == 1:
-        fill_all(*get_color(str(loop_digest[0])))
-
-
-def get_color(digit_to_use_for_color):
-    return number_color_dict[int(digit_to_use_for_color, 16) % 4]
+        fill_all(get_color(str(loop_digest[0])))
 
 
 def job(digest_portion):
     start_time = time.time()
     loop_time = int(start_time)
     loop_digest = totp.generate_digest(loop_time, const[sys.argv[1]], digest_portion)
-
     print(f'  Time is:  {start_time}')
     print(f'Loop time:  {loop_time}')
     print(f'   Digest:  {loop_digest}')
-
     fill_board(loop_digest, digest_portion)
-
     ms_of_loop = 1000 * (time.time() - start_time)
     print(f'       ms:  {str(ms_of_loop)}\n')
 
@@ -116,4 +104,3 @@ print(f'   Time is: {time.time()}\n')
 
 while True:
     schedule.run_pending()
-
